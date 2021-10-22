3df65ff205ad718de290315f842afbf7ed11de26
Yash Bhansali <65901765+Yashbhansali7@users.noreply.github.com>
2021-10-22 16:36:59 +0530

    Delete lib directory

--- lib/location/location_model.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class LocationModel with ChangeNotifier {
-  final double latitude;
-  final double longitude;
-  LocationModel(this.latitude,this.longitude);   
-}
-
